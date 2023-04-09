7634a7c9e710e3402db851699a8e2dd16489d1c3
emmanuel ephrim <emmaephrim@gmail.com>
2023-04-09 04:18:05 +0000

    feat(all): left with items to be added

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text, Link, Divider } from "@chakra-ui/react";
 import background_image from "../images/shop_products/background_image.jpg";
 import shop_banner_image from "../images/shop_products/shop_banner_image.png";
 import shop_profile from "../images/shop_products/shop_profile.png";
@@ -14,30 +14,47 @@ export default function Shope() {
         backgroundSize={"cover"}
         backgroundRepeat={"no-repeat"}
       >
-        <Box px={{ md: "10%" }} py={{ md: "10" }}>
+        <Box
+          px={{ md: "10%" }}
+          pt={{ md: "10" }}
+          pb={{ base: "3px", md: "10" }}
+        >
           <Image src={shop_banner_image} objectFit={"cover"} width={"100%"} />
-          <Box>
+          <Box
+            color={"#164169"}
+            fontFamily={"Poppins"}
+            // color="#164169"
+            fontStyle={"normal"}
+            display={"flex"}
+            px={{ base: "10px" }}
+          >
             <Image
               src={shop_profile}
               borderRadius={"50%"}
               mt={"-8"}
-              width={{ base: "20%", md: "15%" }}
+              width={{ base: "100px", sm: "150px", lg: "160px" }}
+              height={{ base: "100%", md: "" }}
             />
-            <Box>
+            <Box p={"10px"}>
               <Text
-                fontFamily={"Poppins"}
-                color="#164169"
-                fontStyle={"normal"}
-                fontWeight={"700"}
+                fontWeight={"800"}
                 // fontSize={"px"}
               >
                 TRUE BOTANICALS
               </Text>
-              <Text display={"flex"}>
-                About <Image src={info_icon} />
+              <Text display={"flex"} fontSize={"larger"}>
+                About
+                <Image src={info_icon} ml="1" mb={"0.5"} />
+              </Text>
+              <Text width={{ md: "90%", lg: "70%" }}>
+                Liberate your glow with wildly clean ingredients. 100 % NON
+                TOXIC, MADE SAFE® CERTIFIED, CRUELTY -FREE Our clean and
+                sustainable products are clinically proven to work at the
+                highest standards.
               </Text>
             </Box>
           </Box>
+          <hr style={{ height: "2px", backgroundColor: "black" }}></hr>
         </Box>
       </Box>
     </Box>
